dc66fd15362299b2fe90063c77659280369b5a2d
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-08-10 01:54:03 +0530

    removed unwanted pages

--- my-portfolio/src/app/experience/page.js
+++
@@ -1,7 +0,0 @@
-export default function Experience() {
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center flex-[75%]">
-            <h1>Experience</h1>
-        </div>
-    );
-}
